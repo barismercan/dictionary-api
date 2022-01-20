47802daaa7ba415c2448f8152d618e2279056277
LAPTOP-KNBC20AL\Baris <bmercan_16@hotmail.com>
2022-01-20 07:05:05 +0300

    bug fix

--- src/main/java/com/example/demo/WordController.java
+++ src/main/java/com/example/demo/WordController.java
@@ -2,12 +2,9 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.http.HttpResponse;
 
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/demo")
